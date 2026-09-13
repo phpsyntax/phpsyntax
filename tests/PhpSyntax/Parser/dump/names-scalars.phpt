dg/dresscode

tests: dump fixtures for every syntax category, regenerated by tests/_update-tests.php; the agent guide on build coverage and value stack arrays

tests/PhpSyntax/Parser/dump/names-scalars.phpt
@@ -1,0 +1,127 @@
+<?php declare(strict_types=1);
+
+// names, identifiers, scalars and constants
+
+use DressCode\Tools\Dumper;
+use PhpSyntax\Parser\Parser;
+use Tester\Assert;
+
+require __DIR__ . '/../../../bootstrap.php';
+
+$input = <<<'XX'
+	<?php
+	$a = 1 + 0x1F + 0b11 + 0o17 + 1_000;
+	$b = .5 + 1.5e3;
+	$c = 'single' . "double" . b'binary';
+	$d = FOO . \Foo\BAR . namespace\BAZ . __LINE__ . __CLASS__;
+	$e = true && null;
+	__halt_compiler(); data
+	XX;
+
+$file = (new Parser)->parse($input);
+Assert::same($input, (string) $file);
+Assert::same(loadExpected(__FILE__, __COMPILER_HALT_OFFSET__), Dumper::dump($file));
+
+__halt_compiler();
+FileNode
+  stmts: NodeList
+    - ExpressionStatementNode
+      expr: AssignNode
+        var: VariableNode
+          name: Variable "$a"  <OpenTag"<?php\n"  >Whitespace" "
+        operator: '=' "="  >Whitespace" "
+        expr: BinaryNode
+          left: BinaryNode
+            left: BinaryNode
+              left: BinaryNode
+                left: IntegerNode
+                  token: Integer "1"  >Whitespace" "
+                operator: '+' "+"  >Whitespace" "
+                right: IntegerNode
+                  token: Integer "0x1F"  >Whitespace" "
+              operator: '+' "+"  >Whitespace" "
+              right: IntegerNode
+                token: Integer "0b11"  >Whitespace" "
+            operator: '+' "+"  >Whitespace" "
+            right: IntegerNode
+              token: Integer "0o17"  >Whitespace" "
+          operator: '+' "+"  >Whitespace" "
+          right: IntegerNode
+            token: Integer "1_000"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - ExpressionStatementNode
+      expr: AssignNode
+        var: VariableNode
+          name: Variable "$b"  >Whitespace" "
+        operator: '=' "="  >Whitespace" "
+        expr: BinaryNode
+          left: FloatNode
+            token: Float ".5"  >Whitespace" "
+          operator: '+' "+"  >Whitespace" "
+          right: FloatNode
+            token: Float "1.5e3"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - ExpressionStatementNode
+      expr: AssignNode
+        var: VariableNode
+          name: Variable "$c"  >Whitespace" "
+        operator: '=' "="  >Whitespace" "
+        expr: BinaryNode
+          left: BinaryNode
+            left: StringNode
+              token: ConstantEncapsedString "'single'"  >Whitespace" "
+            operator: '.' "."  >Whitespace" "
+            right: StringNode
+              token: ConstantEncapsedString "\"double\""  >Whitespace" "
+          operator: '.' "."  >Whitespace" "
+          right: StringNode
+            token: ConstantEncapsedString "b'binary'"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - ExpressionStatementNode
+      expr: AssignNode
+        var: VariableNode
+          name: Variable "$d"  >Whitespace" "
+        operator: '=' "="  >Whitespace" "
+        expr: BinaryNode
+          left: BinaryNode
+            left: BinaryNode
+              left: BinaryNode
+                left: ConstantFetchNode
+                  name: NameNode
+                    token: Identifier "FOO"  >Whitespace" "
+                operator: '.' "."  >Whitespace" "
+                right: ConstantFetchNode
+                  name: NameNode
+                    token: NameFullyQualified "\\Foo\\BAR"  >Whitespace" "
+              operator: '.' "."  >Whitespace" "
+              right: ConstantFetchNode
+                name: NameNode
+                  token: NameRelative "namespace\\BAZ"  >Whitespace" "
+            operator: '.' "."  >Whitespace" "
+            right: MagicConstantNode
+              token: MagicLine "__LINE__"  >Whitespace" "
+          operator: '.' "."  >Whitespace" "
+          right: MagicConstantNode
+            token: MagicClass "__CLASS__"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - ExpressionStatementNode
+      expr: AssignNode
+        var: VariableNode
+          name: Variable "$e"  >Whitespace" "
+        operator: '=' "="  >Whitespace" "
+        expr: BinaryNode
+          left: ConstantFetchNode
+            name: NameNode
+              token: Identifier "true"  >Whitespace" "
+          operator: BooleanAnd "&&"  >Whitespace" "
+          right: ConstantFetchNode
+            name: NameNode
+              token: Identifier "null"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - HaltCompilerNode
+      haltKeyword: HaltCompiler "__halt_compiler"
+      openParen: '(' "("
+      closeParen: ')' ")"
+      semicolon: ';' ";"
+      data: HaltCompilerData " data"
+  eof: EndOfFile ""

tests/PhpSyntax/Parser/dump/namespaces.phpt
@@ -1,0 +1,171 @@
+<?php declare(strict_types=1);
+
+// namespaces (nested form), use imports, const, declare, inline HTML
+
+use DressCode\Tools\Dumper;
+use PhpSyntax\Parser\Parser;
+use Tester\Assert;
+
+require __DIR__ . '/../../../bootstrap.php';
+
+$input = <<<'XX'
+	<html>
+	<?php declare(strict_types=1);
+	namespace A\B;
+	use C\D, E\F as G;
+	use function H\i;
+	use const J\K;
+	use L\{M, N as O, function p, const Q,};
+	use function R\{s, t};
+	const U = 1, V = 2;
+	namespace W {
+		$x;
+	}
+	namespace {
+		$y;
+	}
+	XX;
+
+$file = (new Parser)->parse($input);
+Assert::same($input, (string) $file);
+Assert::same(loadExpected(__FILE__, __COMPILER_HALT_OFFSET__), Dumper::dump($file));
+
+__halt_compiler();
+FileNode
+  stmts: NodeList
+    - InlineHtmlNode
+      html: InlineHtml "<html>\n"
+    - DeclareNode
+      declareKeyword: Declare "declare"  <OpenTag"<?php "
+      openParen: '(' "("
+      items: SeparatedNodeList
+        - DeclareItemNode
+          name: IdentifierNode
+            token: Identifier "strict_types"
+          equals: '=' "="
+          value: IntegerNode
+            token: Integer "1"
+      closeParen: ')' ")"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - NamespaceNode
+      namespaceKeyword: Namespace "namespace"  >Whitespace" "
+      name: NameNode
+        token: NameQualified "A\\B"
+      semicolon: ';' ";"  >EndOfLine"\n"
+      stmts: NodeList
+        - UseNode
+          useKeyword: Use "use"  >Whitespace" "
+          items: SeparatedNodeList
+            - UseItemNode
+              name: NameNode
+                token: NameQualified "C\\D"
+            - ',' ","  >Whitespace" "
+            - UseItemNode
+              name: NameNode
+                token: NameQualified "E\\F"  >Whitespace" "
+              asKeyword: As "as"  >Whitespace" "
+              alias: IdentifierNode
+                token: Identifier "G"
+          semicolon: ';' ";"  >EndOfLine"\n"
+        - UseNode
+          useKeyword: Use "use"  >Whitespace" "
+          type: Function "function"  >Whitespace" "
+          items: SeparatedNodeList
+            - UseItemNode
+              name: NameNode
+                token: NameQualified "H\\i"
+          semicolon: ';' ";"  >EndOfLine"\n"
+        - UseNode
+          useKeyword: Use "use"  >Whitespace" "
+          type: Const "const"  >Whitespace" "
+          items: SeparatedNodeList
+            - UseItemNode
+              name: NameNode
+                token: NameQualified "J\\K"
+          semicolon: ';' ";"  >EndOfLine"\n"
+        - GroupUseNode
+          useKeyword: Use "use"  >Whitespace" "
+          prefix: NameNode
+            token: Identifier "L"
+          namespaceSeparator: NamespaceSeparator "\\"
+          openBrace: '{' "{"
+          items: SeparatedNodeList
+            - UseItemNode
+              name: NameNode
+                token: Identifier "M"
+            - ',' ","  >Whitespace" "
+            - UseItemNode
+              name: NameNode
+                token: Identifier "N"  >Whitespace" "
+              asKeyword: As "as"  >Whitespace" "
+              alias: IdentifierNode
+                token: Identifier "O"
+            - ',' ","  >Whitespace" "
+            - UseItemNode
+              type: Function "function"  >Whitespace" "
+              name: NameNode
+                token: Identifier "p"
+            - ',' ","  >Whitespace" "
+            - UseItemNode
+              type: Const "const"  >Whitespace" "
+              name: NameNode
+                token: Identifier "Q"
+            - ',' ","
+          closeBrace: '}' "}"
+          semicolon: ';' ";"  >EndOfLine"\n"
+        - GroupUseNode
+          useKeyword: Use "use"  >Whitespace" "
+          type: Function "function"  >Whitespace" "
+          prefix: NameNode
+            token: Identifier "R"
+          namespaceSeparator: NamespaceSeparator "\\"
+          openBrace: '{' "{"
+          items: SeparatedNodeList
+            - UseItemNode
+              name: NameNode
+                token: Identifier "s"
+            - ',' ","  >Whitespace" "
+            - UseItemNode
+              name: NameNode
+                token: Identifier "t"
+          closeBrace: '}' "}"
+          semicolon: ';' ";"  >EndOfLine"\n"
+        - ConstNode
+          attributes: NodeList
+          constKeyword: Const "const"  >Whitespace" "
+          items: SeparatedNodeList
+            - ConstItemNode
+              name: IdentifierNode
+                token: Identifier "U"  >Whitespace" "
+              equals: '=' "="  >Whitespace" "
+              value: IntegerNode
+                token: Integer "1"
+            - ',' ","  >Whitespace" "
+            - ConstItemNode
+              name: IdentifierNode
+                token: Identifier "V"  >Whitespace" "
+              equals: '=' "="  >Whitespace" "
+              value: IntegerNode
+                token: Integer "2"
+          semicolon: ';' ";"  >EndOfLine"\n"
+    - NamespaceNode
+      namespaceKeyword: Namespace "namespace"  >Whitespace" "
+      name: NameNode
+        token: Identifier "W"  >Whitespace" "
+      openBrace: '{' "{"  >EndOfLine"\n"
+      stmts: NodeList
+        - ExpressionStatementNode
+          expr: VariableNode
+            name: Variable "$x"  <Whitespace"\t"
+          semicolon: ';' ";"  >EndOfLine"\n"
+      closeBrace: '}' "}"  >EndOfLine"\n"
+    - NamespaceNode
+      namespaceKeyword: Namespace "namespace"  >Whitespace" "
+      openBrace: '{' "{"  >EndOfLine"\n"
+      stmts: NodeList
+        - ExpressionStatementNode
+          expr: VariableNode
+            name: Variable "$y"  <Whitespace"\t"
+          semicolon: ';' ";"  >EndOfLine"\n"
+      closeBrace: '}' "}"
+  eof: EndOfFile ""

phpsyntax/phpsyntax

tests/PhpSyntax/Parser/dump/names-scalars.phpt
@@ -1,0 +1,124 @@
+<?php declare(strict_types=1);
+
+// names, identifiers, scalars and constants
+
+use PhpSyntax\Parser;
+use Tester\Assert;
+
+require __DIR__ . '/../../../bootstrap.php';
+
+$input = <<<'XX'
+	<?php
+	$a = 1 + 0x1F + 0b11 + 0o17 + 1_000;
+	$b = .5 + 1.5e3;
+	$c = 'single' . "double" . b'binary';
+	$d = FOO . \Foo\BAR . namespace\BAZ . __LINE__ . __CLASS__;
+	$e = true && null;
+	__halt_compiler(); data
+	XX;
+
+$file = (new Parser)->parse($input);
+Assert::same($input, (string) $file);
+Assert::same(loadExpected(__FILE__, __COMPILER_HALT_OFFSET__), Dumper::dump($file));
+
+__halt_compiler();
+FileNode
+  statements: NodeList
+    - ExpressionStatementNode
+      expression: AssignmentNode
+        target: VariableNode
+          name: Variable "$a"  <OpenTag"<?php\n"  >Whitespace" "
+        operator: '=' "="  >Whitespace" "
+        expression: BinaryOpNode
+          left: BinaryOpNode
+            left: BinaryOpNode
+              left: BinaryOpNode
+                left: IntegerNode
+                  token: Integer "1"  >Whitespace" "
+                operator: '+' "+"  >Whitespace" "
+                right: IntegerNode
+                  token: Integer "0x1F"  >Whitespace" "
+              operator: '+' "+"  >Whitespace" "
+              right: IntegerNode
+                token: Integer "0b11"  >Whitespace" "
+            operator: '+' "+"  >Whitespace" "
+            right: IntegerNode
+              token: Integer "0o17"  >Whitespace" "
+          operator: '+' "+"  >Whitespace" "
+          right: IntegerNode
+            token: Integer "1_000"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - ExpressionStatementNode
+      expression: AssignmentNode
+        target: VariableNode
+          name: Variable "$b"  >Whitespace" "
+        operator: '=' "="  >Whitespace" "
+        expression: BinaryOpNode
+          left: FloatNode
+            token: Float ".5"  >Whitespace" "
+          operator: '+' "+"  >Whitespace" "
+          right: FloatNode
+            token: Float "1.5e3"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - ExpressionStatementNode
+      expression: AssignmentNode
+        target: VariableNode
+          name: Variable "$c"  >Whitespace" "
+        operator: '=' "="  >Whitespace" "
+        expression: BinaryOpNode
+          left: BinaryOpNode
+            left: StringNode
+              token: ConstantEncapsedString "'single'"  >Whitespace" "
+            operator: '.' "."  >Whitespace" "
+            right: StringNode
+              token: ConstantEncapsedString "\"double\""  >Whitespace" "
+          operator: '.' "."  >Whitespace" "
+          right: StringNode
+            token: ConstantEncapsedString "b'binary'"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - ExpressionStatementNode
+      expression: AssignmentNode
+        target: VariableNode
+          name: Variable "$d"  >Whitespace" "
+        operator: '=' "="  >Whitespace" "
+        expression: BinaryOpNode
+          left: BinaryOpNode
+            left: BinaryOpNode
+              left: BinaryOpNode
+                left: ConstantFetchNode
+                  name: NameNode
+                    token: Identifier "FOO"  >Whitespace" "
+                operator: '.' "."  >Whitespace" "
+                right: ConstantFetchNode
+                  name: NameNode
+                    token: NameFullyQualified "\\Foo\\BAR"  >Whitespace" "
+              operator: '.' "."  >Whitespace" "
+              right: ConstantFetchNode
+                name: NameNode
+                  token: NameRelative "namespace\\BAZ"  >Whitespace" "
+            operator: '.' "."  >Whitespace" "
+            right: MagicConstantNode
+              token: MagicLine "__LINE__"  >Whitespace" "
+          operator: '.' "."  >Whitespace" "
+          right: MagicConstantNode
+            token: MagicClass "__CLASS__"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - ExpressionStatementNode
+      expression: AssignmentNode
+        target: VariableNode
+          name: Variable "$e"  >Whitespace" "
+        operator: '=' "="  >Whitespace" "
+        expression: BinaryOpNode
+          left: BooleanNode
+            token: Identifier "true"  >Whitespace" "
+          operator: BooleanAnd "&&"  >Whitespace" "
+          right: NullNode
+            token: Identifier "null"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - HaltCompilerNode
+      haltKeyword: HaltCompiler "__halt_compiler"
+      openParen: '(' "("
+      closeParen: ')' ")"
+      semicolon: ';' ";"
+      data: HaltCompilerData " data"
+  endOfFile: EndOfFile ""

tests/PhpSyntax/Parser/dump/namespaces.phpt
@@ -1,0 +1,170 @@
+<?php declare(strict_types=1);
+
+// namespaces (nested form), use imports, const, declare, inline HTML
+
+use PhpSyntax\Parser;
+use Tester\Assert;
+
+require __DIR__ . '/../../../bootstrap.php';
+
+$input = <<<'XX'
+	<html>
+	<?php declare(strict_types=1);
+	namespace A\B;
+	use C\D, E\F as G;
+	use function H\i;
+	use const J\K;
+	use L\{M, N as O, function p, const Q,};
+	use function R\{s, t};
+	const U = 1, V = 2;
+	namespace W {
+		$x;
+	}
+	namespace {
+		$y;
+	}
+	XX;
+
+$file = (new Parser)->parse($input);
+Assert::same($input, (string) $file);
+Assert::same(loadExpected(__FILE__, __COMPILER_HALT_OFFSET__), Dumper::dump($file));
+
+__halt_compiler();
+FileNode
+  statements: NodeList
+    - InlineHtmlNode
+      html: InlineHtml "<html>\n"
+    - DeclareNode
+      declareKeyword: Declare "declare"  <OpenTag"<?php "
+      openParen: '(' "("
+      items: SeparatedNodeList
+        - DeclareItemNode
+          name: IdentifierNode
+            token: Identifier "strict_types"
+          equals: '=' "="
+          value: IntegerNode
+            token: Integer "1"
+      closeParen: ')' ")"
+      semicolon: ';' ";"  >EndOfLine"\n"
+    - NamespaceNode
+      namespaceKeyword: NamespaceKeyword "namespace"  >Whitespace" "
+      name: NameNode
+        token: NameQualified "A\\B"
+      semicolon: ';' ";"  >EndOfLine"\n"
+      statements: NodeList
+        - UseNode
+          useKeyword: Use "use"  >Whitespace" "
+          items: SeparatedNodeList
+            - UseItemNode
+              name: NameNode
+                token: NameQualified "C\\D"
+            - ',' ","  >Whitespace" "
+            - UseItemNode
+              name: NameNode
+                token: NameQualified "E\\F"  >Whitespace" "
+              asKeyword: As "as"  >Whitespace" "
+              alias: IdentifierNode
+                token: Identifier "G"
+          semicolon: ';' ";"  >EndOfLine"\n"
+        - UseNode
+          useKeyword: Use "use"  >Whitespace" "
+          type: Function "function"  >Whitespace" "
+          items: SeparatedNodeList
+            - UseItemNode
+              name: NameNode
+                token: NameQualified "H\\i"
+          semicolon: ';' ";"  >EndOfLine"\n"
+        - UseNode
+          useKeyword: Use "use"  >Whitespace" "
+          type: Const "const"  >Whitespace" "
+          items: SeparatedNodeList
+            - UseItemNode
+              name: NameNode
+                token: NameQualified "J\\K"
+          semicolon: ';' ";"  >EndOfLine"\n"
+        - UseNode
+          useKeyword: Use "use"  >Whitespace" "
+          prefix: NameNode
+            token: Identifier "L"
+          namespaceSeparator: NamespaceSeparator "\\"
+          openBrace: '{' "{"
+          items: SeparatedNodeList
+            - UseItemNode
+              name: NameNode
+                token: Identifier "M"
+            - ',' ","  >Whitespace" "
+            - UseItemNode
+              name: NameNode
+                token: Identifier "N"  >Whitespace" "
+              asKeyword: As "as"  >Whitespace" "
+              alias: IdentifierNode
+                token: Identifier "O"
+            - ',' ","  >Whitespace" "
+            - UseItemNode
+              type: Function "function"  >Whitespace" "
+              name: NameNode
+                token: Identifier "p"
+            - ',' ","  >Whitespace" "
+            - UseItemNode
+              type: Const "const"  >Whitespace" "
+              name: NameNode
+                token: Identifier "Q"
+            - ',' ","
+          closeBrace: '}' "}"
+          semicolon: ';' ";"  >EndOfLine"\n"
+        - UseNode
+          useKeyword: Use "use"  >Whitespace" "
+          type: Function "function"  >Whitespace" "
+          prefix: NameNode
+            token: Identifier "R"
+          namespaceSeparator: NamespaceSeparator "\\"
+          openBrace: '{' "{"
+          items: SeparatedNodeList
+            - UseItemNode
+              name: NameNode
+                token: Identifier "s"
+            - ',' ","  >Whitespace" "
+            - UseItemNode
+              name: NameNode
+                token: Identifier "t"
+          closeBrace: '}' "}"
+          semicolon: ';' ";"  >EndOfLine"\n"
+        - ConstNode
+          attributes: NodeList
+          constKeyword: Const "const"  >Whitespace" "
+          items: SeparatedNodeList
+            - ConstItemNode
+              name: IdentifierNode
+                token: Identifier "U"  >Whitespace" "
+              equals: '=' "="  >Whitespace" "
+              value: IntegerNode
+                token: Integer "1"
+            - ',' ","  >Whitespace" "
+            - ConstItemNode
+              name: IdentifierNode
+                token: Identifier "V"  >Whitespace" "
+              equals: '=' "="  >Whitespace" "
+              value: IntegerNode
+                token: Integer "2"
+          semicolon: ';' ";"  >EndOfLine"\n"
+    - NamespaceNode
+      namespaceKeyword: NamespaceKeyword "namespace"  >Whitespace" "
+      name: NameNode
+        token: Identifier "W"  >Whitespace" "
+      openBrace: '{' "{"  >EndOfLine"\n"
+      statements: NodeList
+        - ExpressionStatementNode
+          expression: VariableNode
+            name: Variable "$x"  <Whitespace"\t"
+          semicolon: ';' ";"  >EndOfLine"\n"
+      closeBrace: '}' "}"  >EndOfLine"\n"
+    - NamespaceNode
+      namespaceKeyword: NamespaceKeyword "namespace"  >Whitespace" "
+      openBrace: '{' "{"  >EndOfLine"\n"
+      statements: NodeList
+        - ExpressionStatementNode
+          expression: VariableNode
+            name: Variable "$y"  <Whitespace"\t"
+          semicolon: ';' ";"  >EndOfLine"\n"
+      closeBrace: '}' "}"
+  endOfFile: EndOfFile ""